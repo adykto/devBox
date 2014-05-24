ed24a06f98515ee096e0d888f564f7b62ea308be
Alonso Mendez <alonsomendez@gmail.com>
2014-05-24 06:39:44 -0700

    Adding apache2 files

---
+++ puppet/manifests/lamp.pp
@@ -0,0 +1,15 @@
+stage { 'pre':
+  before => Stage['main']
+}
+
+class { 'baseconfig':
+  stage => 'pre'
+}
+
+File {
+  owner => 'root',
+  group => 'root',
+  mode  => '0644',
+}
+
+include baseconfig, apache, mysql, php, apache_vhosts
