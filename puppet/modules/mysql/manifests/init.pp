6fa56b9e265aa8871c0e1cacf14023b9d2454682
Alonso Mendez <alonsomendez@gmail.com>
2014-05-24 07:10:23 -0700

    Added mysql

---
+++ puppet/modules/mysql/manifests/init.pp
@@ -0,0 +1,29 @@
+class mysql {
+  package { ['mysql-server']:
+    ensure => present;
+  }
+
+  service { 'mysql':
+    ensure  => running,
+    require => Package['mysql-server'];
+  }
+
+  file { '/etc/mysql/my.cnf':
+    source  => 'puppet:///modules/mysql/my.cnf',
+    require => Package['mysql-server'],
+    notify  => Service['mysql'];
+  }
+
+  exec { 'set-mysql-password':
+    unless  => 'mysqladmin -uroot -proot status',
+    command => "mysqladmin -uroot password root",
+    path    => ['/bin', '/usr/bin'],
+    require => Service['mysql'];
+  }
+
+  exec { 'load-dynamic-sql':
+    command => 'mysql -u root -proot < /vagrant/sites/dynamic.sql',
+    path    => ['/bin', '/usr/bin'],
+    require => Exec['set-mysql-password'];
+  }
+}
