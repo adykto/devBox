1e2bbe928f2c3e0a8760f359a1a33c6c50ac91d5
Alonso Mendez <alonsomendez@gmail.com>
2014-06-02 03:26:29 +0000

    Adding meanio generator

--- puppet/modules/nodejs/manifests/init.pp
+++ puppet/modules/nodejs/manifests/init.pp
@@ -6,7 +6,7 @@ class nodejs {
   }
 
   exec { 'npm install stuff':
-    command => '/usr/bin/npm install -g express express-generator jade yo bower grunt grunt-cli grunt-contrib-clean grunt-contrib-concat grunt-contrib-copy grunt-contrib-cssmin grunt-contrib-jshint grunt-contrib-nodeunit grunt-contrib-uglify grunt-contrib-watch',
+    command => '/usr/bin/npm install -g express express-generator jade yo monk mongoose bower grunt grunt-cli grunt-contrib-clean grunt-contrib-concat grunt-contrib-copy grunt-contrib-cssmin grunt-contrib-jshint grunt-contrib-nodeunit grunt-contrib-uglify grunt-contrib-watch meanio',
     require => Package['npm'];
   }
 }
