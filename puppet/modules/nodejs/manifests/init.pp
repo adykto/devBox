773eded8243a43ec30021192dc6aec721b9c55f9
Alonso Mendez <alonsomendez@gmail.com>
2014-06-07 21:47:06 +0000

    New mirror lookup for npm install

--- puppet/modules/nodejs/manifests/init.pp
+++ puppet/modules/nodejs/manifests/init.pp
@@ -5,6 +5,11 @@ class nodejs {
     require => Package['npm'];
   }
 
+  exec { 'npmconfig':
+    command => '/usr/bin/npm config set registry http://registry.npmjs.org/',
+    require => Package['npm'];
+  }
+
   exec { 'npm install stuff':
     command => '/usr/bin/npm install -g express express-generator jade yo monk mongoose karma jasmine-node karma-jasmine phantomjs mocha bower grunt grunt-cli grunt-contrib-clean grunt-contrib-concat grunt-contrib-copy grunt-contrib-cssmin grunt-contrib-jshint grunt-contrib-nodeunit grunt-contrib-uglify grunt-contrib-watch grunt-mocha meanio --save-dev',
     require => Package['npm'];
